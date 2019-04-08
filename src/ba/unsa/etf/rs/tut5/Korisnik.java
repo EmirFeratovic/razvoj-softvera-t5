5c1bc2da6210b993b6ffd65b7fae3bcf2413fa28
EmirFeratovic <eferatovic1>
2019-04-08 10:53:00 +0200

    Initial commit

---
+++ src/ba/unsa/etf/rs/tut5/Korisnik.java
@@ -0,0 +1,75 @@
+package ba.unsa.etf.rs.tut5;
+
+import javafx.beans.property.SimpleStringProperty;
+
+public class Korisnik {
+    private SimpleStringProperty ime,prezime,email,username,password;
+
+    public Korisnik(SimpleStringProperty ime, SimpleStringProperty prezime, SimpleStringProperty email, SimpleStringProperty username, SimpleStringProperty password) {
+        this.ime = ime;
+        this.prezime = prezime;
+        this.email = email;
+        this.username = username;
+        this.password = password;
+    }
+
+    public String getIme() {
+        return ime.get();
+    }
+
+    public SimpleStringProperty imeProperty() {
+        return ime;
+    }
+
+    public void setIme(String ime) {
+        this.ime.set(ime);
+    }
+
+    public String getPrezime() {
+        return prezime.get();
+    }
+
+    public SimpleStringProperty prezimeProperty() {
+        return prezime;
+    }
+
+    public void setPrezime(String prezime) {
+        this.prezime.set(prezime);
+    }
+
+    public String getEmail() {
+        return email.get();
+    }
+
+    public SimpleStringProperty emailProperty() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email.set(email);
+    }
+
+    public String getUsername() {
+        return username.get();
+    }
+
+    public SimpleStringProperty usernameProperty() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username.set(username);
+    }
+
+    public String getPassword() {
+        return password.get();
+    }
+
+    public SimpleStringProperty passwordProperty() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password.set(password);
+    }
+}
